d8b461dabe15f619b691a0aeb43a326e2995856c
caofengbin <1050430934@qq.com>
2017-09-27 10:01:13 +0800

    add code 0927

---
+++ src/com/algorithm/linkedlist/fengbin/Problem_20170927_RemoveLastKthNode.java
@@ -0,0 +1,20 @@
+package com.algorithm.linkedlist.fengbin;
+
+/**
+ * 分别实现删除单链表和双链表中倒数第K个节点的操作
+ * 
+ * 要求：
+ * 如果链表长度为N，要求时间复杂度达到O(N)，额外空间复杂度达到O(1)
+ * @author fengbincao
+ */
+public class Problem_20170927_RemoveLastKthNode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
