51c04dde71b7ccc87b3d8c413d6f97126b9420ea
caofengbin <1050430934@qq.com>
2017-10-06 20:55:14 +0800

    add code about linklist

---
+++ src/com/algorithm/linkedlist/diana/Problem_20171007_ReversePartList.java
@@ -0,0 +1,29 @@
+package com.algorithm.linkedlist.diana;
+/**
+ * 问题描述:
+ * 反转部分单向链表。
+ * 
+ * 详细描述：
+ * 给定一个单向链表的头节点head，以及两个整数from和to，在单向链表上把第from个节点到第to个节点这一部分进行反转
+ * 
+ * 测试用例
+ * (1) 1->2->3->4->5->null，from=2,to=4,调整后的结果为： 1->4->3->2->5->null
+ * (2) 1->2->3->null，from=1,to=3,调整后的结果为： 3->2->1->null
+ * 
+ * 要求：
+ * (1) 对于长度为N的链表，要求时间复杂度为O(N)，额外的空间复杂度为O(1)
+ * (2) 对于不满足1<=from<=to<=N的情况，则不用调整
+ * 
+ * @author fengbincao
+ */
+public class Problem_20171007_ReversePartList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
