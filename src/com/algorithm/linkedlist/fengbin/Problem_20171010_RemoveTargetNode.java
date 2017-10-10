49223528e4a120de3694f62cce235ad512836f57
caofengbin <1050430934@qq.com>
2017-10-10 09:59:59 +0800

    add code about this day

---
+++ src/com/algorithm/linkedlist/fengbin/Problem_20171010_RemoveTargetNode.java
@@ -0,0 +1,13 @@
+package com.algorithm.linkedlist.fengbin;
+
+public class Problem_20171010_RemoveTargetNode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
