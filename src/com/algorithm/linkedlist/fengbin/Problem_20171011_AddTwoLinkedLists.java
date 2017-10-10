5509167900aaf0059de352dcc4dfc817b1382cd7
caofengbin <1050430934@qq.com>
2017-10-10 14:21:19 +0800

    init AddTwoLinkedLists code

---
+++ src/com/algorithm/linkedlist/fengbin/Problem_20171011_AddTwoLinkedLists.java
@@ -0,0 +1,25 @@
+package com.algorithm.linkedlist.fengbin;
+/**
+ * 问题描述:
+ * 两个单链表生成相加后的链表
+ * 
+ * 详细描述:
+ * 假设链表中每一个节点的值都在0--9之间，那么链表整体就可以代表一个整数。
+ * 例如:9->3->7，可以代表整数937。
+ * 给定两个这种链表的头节点head1和head2，生成代表这两个整数相加值的结果链表。
+ * 例如，链表1为9->3->7->null，链表2为6->3。
+ * 最后的结果为:1->0->0->0->null
+ * 
+ * @author fengbincao
+ */
+public class Problem_20171011_AddTwoLinkedLists {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
