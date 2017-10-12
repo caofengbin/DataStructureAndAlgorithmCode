f1ebbac2444ff24e9f7eccbd0d05c916ff5ae29a
caofengbin <1050430934@qq.com>
2017-10-12 20:26:21 +0800

    add code

--- src/com/algorithm/linkedlist/fengbin/LinkListSelectedSort.java
+++ src/com/algorithm/linkedlist/fengbin/LinkListSelectedSort.java
@@ -7,8 +7,47 @@ package com.algorithm.linkedlist.fengbin;
  */
 public class LinkListSelectedSort {
 
+	/**
+	 * 思路： 从未排序的的部分找到最小值，然后放在排好序的尾部，逐步将未排序的部分逐渐缩小，组装成一个新的链表即可
+	 * 
+	 * @param headNode
+	 *            待排序的头结
+	 * @return
+	 */
 	public static LinkedNode selectedSort(LinkedNode headNode) {
+		LinkedNode curNode = null;
+		LinkedNode preNode = null;
+		LinkedNode minNode = null;
+		LinkedNode newHeadNode = null;
 		return null;
 
+	}
+
+	/**
+	 * 返回链表中节点值最小的元素的前一个元素的前一个节点
+	 * 
+	 * @param headNode
+	 *            待处理的链表的头结点
+	 * @return 节点值最小的元素的前一个元素的前一个节点
+	 */
+	private static LinkedNode getSmallestNode(LinkedNode headNode) {
+		LinkedNode curNode = headNode.next;
+		LinkedNode preNode = headNode;
+
+		LinkedNode smallNode = headNode;
+		LinkedNode preSmallNode = null;
+
+		while (curNode != null) {
+
+			if (curNode.data < smallNode.data) {
+				smallNode = curNode;
+				preSmallNode = preNode;
+			}
+
+			preNode = curNode;
+			curNode = curNode.next;
+		}
+
+		return preSmallNode;
 	}
 }
