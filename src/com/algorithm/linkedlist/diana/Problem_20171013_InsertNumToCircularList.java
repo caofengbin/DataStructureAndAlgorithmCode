6037edce5bfe1f728cb92ede35b41c0c557cb2f8
caofengbin <1050430934@qq.com>
2017-10-13 13:22:38 +0800

    add today code

---
+++ src/com/algorithm/linkedlist/diana/Problem_20171013_InsertNumToCircularList.java
@@ -0,0 +1,22 @@
+package com.algorithm.linkedlist.diana;
+
+/**
+ * 向有序的环形单链表中插入新节点
+ * 
+ * 详细描述:
+ * 一个环形单链表从头节点head开始不降序，同时由最后的节点指回头节点。给定这样一个单链表的头结点head和一个整数num，请生成节点
+ * 值为num的新节点，并插入到环形链表中，保证调整后的链表有序。
+ * 
+ * @author fengbincao
+ */
+public class Problem_20171013_InsertNumToCircularList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
