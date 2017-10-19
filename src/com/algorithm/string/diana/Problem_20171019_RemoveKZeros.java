30654229f45971c7276081c33ecd93ec7a7b8ec7
caofengbin <1050430934@qq.com>
2017-10-19 08:32:28 +0800

    add code

---
+++ src/com/algorithm/string/diana/Problem_20171019_RemoveKZeros.java
@@ -0,0 +1,21 @@
+package com.algorithm.string.diana;
+
+/**
+ * 问题描述: 给定一个字符串str和一个整数k，如果str中正好有连续的k个‘0’字符出现，把k个‘0’字符去除，返回处理后的字符串
+ * 
+ * 测试用例： 
+ * str=‘A00B’，k=2，返回“AB” 
+ * str=‘A0000B000’，k=3，返回“A0000B”
+ * 
+ * @author fengbincao
+ */
+public class Problem_20171019_RemoveKZeros {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
