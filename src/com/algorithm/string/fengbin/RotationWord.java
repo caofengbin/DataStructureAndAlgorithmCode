d0d07d7ddcd4b2ce57b71406b96f477dc02de6ea
caofengbin <1050430934@qq.com>
2017-10-21 21:21:17 +0800

    update code

---
+++ src/com/algorithm/string/fengbin/RotationWord.java
@@ -0,0 +1,23 @@
+package com.algorithm.string.fengbin;
+
+/**
+ * 判断两个字符串是否互为旋转词
+ * 
+ * @author caofengbin
+ *
+ */
+public class RotationWord {
+
+	/**
+	 * 判断两个单词是否互为旋转词
+	 * 
+	 * @param str1
+	 *            待旋转的String1
+	 * @param str2
+	 *            待旋转的String2
+	 * @return true表示是旋转词
+	 */
+	public static boolean isRotationWord(String str1, String str2) {
+		return false;
+	}
+}
