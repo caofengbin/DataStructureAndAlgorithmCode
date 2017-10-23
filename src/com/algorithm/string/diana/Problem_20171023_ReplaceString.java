df342e35d448b1e230ba8b6e4d19268f6ab69871
caofengbin <1050430934@qq.com>
2017-10-23 09:57:37 +0800

    updata description about replaca string

---
+++ src/com/algorithm/string/diana/Problem_20171023_ReplaceString.java
@@ -0,0 +1,27 @@
+package com.algorithm.string.fengbin;
+/**
+ * 问题描述:
+ * 替换字符串中连续出现的指定字符串
+ * 
+ * 详细描述:
+ * 给定三个连续的字符串str,from和to，把str中所有的from字符串全部替换为to字符串，
+ * 对于连续出现from的部分要求只替换成一个to字符串，返回最终的结果字符串。
+ * 
+ * 测试用例:
+ * (1) str="123abc",from="abc",to="4567",返回"1234567"
+ * (2) str="123",from="abc",to="456",返回"123"
+ * (3) str="123abcabc",from="abc",to="X",返回"123X"
+ * 
+ * @author fengbincao
+ */
+public class Problem_20171023_ReplaceString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
