d8d95679a72cefa1371358a2f6ece5ac4e66fdcc
caofengbin <1050430934@qq.com>
2017-10-24 09:51:51 +0800

    add today code

---
+++ src/com/algorithm/string/diana/Problem_20171024_ParenthesesString.java
@@ -0,0 +1,26 @@
+package com.algorithm.string.fengbin;
+/**
+ * 问题描述:
+ * 给定一个字符串str，判断是不是整体有效的括号字符串。
+ * 
+ * 测试用例:
+ * (1) str="()",返回true。
+ * (2) str="(()())",返回true。
+ * (3) str="(())",返回true。
+ * (4) str="())",返回false。
+ * (5) str="()(",返回false。
+ * (6) str="()a()",返回false。
+ * 
+ * @author fengbincao
+ */
+public class Problem_20171024_ParenthesesString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
