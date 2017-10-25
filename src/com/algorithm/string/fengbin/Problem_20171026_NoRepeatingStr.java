1f998d7e4d6f0876f9a7ba718105ea9eb87da0be
caofengbin <1050430934@qq.com>
2017-10-25 21:11:33 +0800

    add code about no repeats son string

---
+++ src/com/algorithm/string/fengbin/Problem_20171026_NoRepeatingStr.java
@@ -0,0 +1,23 @@
+package com.algorithm.string.fengbin;
+/**
+ * 找到字符串的最长无重复子串
+ * 
+ * 给定一个字符串str,返回str的最长无重复子串的长度。
+ * 
+ * 测试用例:
+ * str="abcd"，返回4.
+ * str="aabcb"，最长无重复子串为"abc"， 返回3
+ * 
+ * @author fengbincao
+ */
+public class Problem_20171026_NoRepeatingStr {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
