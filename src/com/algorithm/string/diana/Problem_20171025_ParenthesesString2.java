3625081f268ac526c44f416e03369cb2f41fd863
caofengbin <1050430934@qq.com>
2017-10-25 21:04:03 +0800

    add toady codes

---
+++ src/com/algorithm/string/diana/Problem_20171025_ParenthesesString2.java
@@ -0,0 +1,23 @@
+package com.algorithm.string.diana;
+/**
+ * 题目Problem_20171024_ParenthesesString的进阶版本
+ * 给定一个括号字符串，返回最长的有效括号子串
+ * 
+ * 测试用例:
+ * str="(()())"，返回6.
+ * str="())"，返回2.
+ * str="()(()()("，返回4.
+ * 
+ * @author fengbincao
+ */
+public class Problem_20171025_ParenthesesString2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
