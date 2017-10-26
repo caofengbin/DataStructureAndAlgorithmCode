95c97a449dd274942db10913a920a584d91571ee
caofengbin <1050430934@qq.com>
2017-10-26 10:07:01 +0800

    implements codes

--- src/com/algorithm/string/fengbin/ParenthesesString.java
+++ src/com/algorithm/string/fengbin/ParenthesesString.java
@@ -2,6 +2,13 @@ package com.algorithm.string.fengbin;
 
 public class ParenthesesString {
 
+	/**
+	 * 给定一个字符串str，判断是不是整体有效的括号字符串
+	 * 
+	 * @param originStr
+	 *            待判断的字符串
+	 * @return true表示符合条件
+	 */
 	public static boolean judgeParentheses(String originStr) {
 		if (isLegalStr(originStr)) {
 			char[] charArrays = originStr.toCharArray();
@@ -17,9 +24,10 @@ public class ParenthesesString {
 						return false;
 					}
 				}
-			
+
 			}
-			
+
+			// 全部扫描匹配完成，判断数量是否相等
 			if (leftBracketsNum != 0) {
 				return false;
 			} else {
@@ -30,12 +38,12 @@ public class ParenthesesString {
 		}
 	}
 
+	/**
+	 * 通过正则限定只允许传入指定的字符串
+	 */
 	private static boolean isLegalStr(String originStr) {
 		String regex1 = "^[()]+";
 		return originStr.matches(regex1);
 	}
 
-	public static void main(String[] args) {
-		System.out.println(judgeParentheses("()())"));
-	}
 }
--- src/com/algorithm/string/fengbin/Problem_20171024_ParenthesesString.java
+++ src/com/algorithm/string/fengbin/Problem_20171024_ParenthesesString.java
@@ -19,7 +19,41 @@ public class Problem_20171024_ParenthesesString {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String originStr1 = "()";
+		System.out.println("字符串: " + originStr1 + 
+				(ParenthesesString.judgeParentheses(originStr1) ? " 是" : " 不是")
+				+ "整体有效的括号字符串");
+		System.out.println();
 		
+		String originStr2 = "(()())";
+		System.out.println("字符串: " + originStr2 + 
+				(ParenthesesString.judgeParentheses(originStr2) ? " 是" : " 不是")
+				+ "整体有效的括号字符串");
+		System.out.println();
+		
+		String originStr3 = "(())";
+		System.out.println("字符串: " + originStr3 + 
+				(ParenthesesString.judgeParentheses(originStr3) ? " 是" : " 不是")
+				+ "整体有效的括号字符串");
+		System.out.println();
+		
+		String originStr4 = "())";
+		System.out.println("字符串: " + originStr4 + 
+				(ParenthesesString.judgeParentheses(originStr4) ? " 是" : " 不是")
+				+ "整体有效的括号字符串");
+		System.out.println();
+		
+		String originStr5 = "()(";
+		System.out.println("字符串: " + originStr5 + 
+				(ParenthesesString.judgeParentheses(originStr5) ? " 是" : " 不是")
+				+ "整体有效的括号字符串");
+		System.out.println();
+		
+		String originStr6 = "()a()";
+		System.out.println("字符串: " + originStr6 + 
+				(ParenthesesString.judgeParentheses(originStr6) ? " 是" : " 不是")
+				+ "整体有效的括号字符串");
+		System.out.println();
 	}
 
 }
